76023fc9dfe023ba87577c44ae5ea42f99de7858
Jobrie Du Preez <jobrie.dupreez@gmail.com>
2023-07-31 15:59:36 +0200

    Revert "[#3]"
    This reverts commit b56f47f9a94f384a56b327897312121c0efab7e5.

--- tut0/Greeter.java
+++ tut0/Greeter.java
@@ -1,15 +1,5 @@
 public class Greeter {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            String name = args[0];
-            greeter(name);
-        } else {
-            System.out.println("Please provide your name as a command-line argument.");
-        }
-    }
-
-    public static void greeter(String name) {
-        System.out.println("Hello, " + name + "!");
+        // Empty main function
     }
 }
-
