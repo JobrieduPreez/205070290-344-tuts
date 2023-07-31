dd85a655a1c00b7f5d650b08f75a9f11e47fe828
Jobrie Du Preez <jobrie.dupreez@gmail.com>
2023-07-31 16:16:36 +0200

    [#4]

--- tut0/Greeter.java
+++ tut0/Greeter.java
@@ -1,5 +1,15 @@
 public class Greeter {
     public static void main(String[] args) {
-        // Empty main function
+        if (args.length > 0) {
+            String name = args[0];
+            greeter(name);
+        } else {
+            System.out.println("Please provide your name as a command-line argument.");
+        }
+    }
+
+    public static void greeter(String name) {
+        System.out.println("Hello, " + name + "!");
     }
 }
+
